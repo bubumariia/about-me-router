bf63313e045d002006928134b98b981b5db991c3
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-10-07 12:32:25 +0300

    first commit, About Me project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
+import Tools from "./components/Tools/Tools";
+import MyChoices from "./components/MyChoices/MyChoices";
+import Websites from "./components/Websites/Websites-to-follow";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/tools">Tools</Link>
+            </li>
+            <li>
+              <Link to="/choices">My Choices</Link>
+            </li>
+            <li>
+              <Link to="/websites">Websites to follow</Link>
+            </li>
+            <li>
+              <a href="https://learn.seytech.co/" target="blank">
+                Seytech
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/tools" element={<Tools />}></Route>
+          <Route path="/choices" element={<MyChoices />}></Route>
+          <Route path="/websites" element={<Websites />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
